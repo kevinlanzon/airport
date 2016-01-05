dae3403cc1b0c5ea0416826b24ba3f67f1b73c2d
Kevin Lanzon <kevinlanzon@gmail.com>
2016-01-05 21:20:37 +0000

    prevents planes landing/taking off when weather is stormy

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -5,6 +5,7 @@ describe 'User stories' do
   it 'so planes land at airport, instruct plane to land' do
     airport = Airport.new(20)
     plane = Plane.new
+    allow(airport).to receive(:stormy?).and_return false
     expect { airport.land(plane) }.not_to raise_error
   end
 
@@ -20,10 +21,25 @@ describe 'User stories' do
   # As an air traffic controller
   # So I can avoid collisions
   # I want to prevent planes landing when my aiport is full
-  it 'should not allow planes to land when airport is full' do
-    airport = Airport.new(20)
-    plane = Plane.new
-    20.times { airport.land(plane) }
-    expect { airport.land(plane) }.to raise_error 'Cannot land plane: airport full'
+  context 'when airport is full' do
+    it 'does not allow planes to land' do
+      airport = Airport.new(20)
+      plane = Plane.new
+      allow(airport).to receive(:stormy?).and_return false
+      20.times { airport.land(plane) }
+      expect { airport.land(plane) }.to raise_error 'Cannot land plane: airport full'
+    end
+  end
+
+  # As an air traffic controller
+  # So I can avoid accidents
+  # I want to prevent planes landing or taking off when the weather is stormy
+  context 'when weather is stormy' do
+    it 'does not allow planes to land' do
+      airport = Airport.new(20)
+      plane = Plane.new
+      allow(airport).to receive(:stormy?).and_return true
+      expect { airport.land(plane) }.to raise_error 'Cannot land plane: weather is stormy'
+    end
   end
 end
