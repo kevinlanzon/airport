58b09b0c686f75e8bf513b93b134cdc7fde062a6
Kevin Lanzon <kevinlanzon@gmail.com>
2016-01-05 20:10:48 +0000

    planes can take off

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -8,4 +8,12 @@ describe 'User stories' do
     expect { airport.land(plane) }.not_to raise_error
   end
 
+  # As an air traffic controller
+  # So planes can take off safely from my airport
+  # I want to instruct a plane to take off
+  it 'so planes take off from airport, instruct planes to take off' do
+    airport = Airport.new
+    plane = Plane.new
+    expect { airport.take_off(plane) }.not_to raise_error
+  end
 end
